ea3c94fcc064809b5bb9dbd5fedcb8f9fcb025f2
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-11-05 13:40:30 +0300

    (feat): Show Loader on page loading

--- app/hooks/Loader.tsx
+++ app/hooks/Loader.tsx
@@ -2,8 +2,13 @@ import { useState, useEffect } from 'react';
 
 const Loader = (minLoadingTime: number = 1500) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [hasMounted, setHasMounted] = useState<boolean>(false);
 
   useEffect(() => {
+    // Set the mounted state after the initial render
+    setHasMounted(true);
+
+    // Promise for minimum loading time
     const minLoadingPromise = new Promise<void>((resolve) => setTimeout(resolve, minLoadingTime));
     const fontLoading = document.fonts.ready;
 
@@ -11,7 +16,8 @@ const Loader = (minLoadingTime: number = 1500) => {
     Promise.all([minLoadingPromise, fontLoading]).then(() => setIsLoading(false));
   }, [minLoadingTime]);
 
-  return isLoading;
+  // Return true if loading is in progress or if the component has not yet mounted
+  return isLoading || !hasMounted;
 };
 
 export default Loader;
