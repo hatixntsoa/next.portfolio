1b5682b72aebaa7c0e16d13702b4e7735340c752
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-11-05 13:23:29 +0300

    (feat): Font loading prioritized

---
+++ app/(root)/_document.tsx
@@ -0,0 +1,30 @@
+// app/(root)/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/NerdFont.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
