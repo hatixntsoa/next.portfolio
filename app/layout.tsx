536069978385974aaa71878e33d950d5f9e4b919
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-11-06 20:47:44 +0300

    fix: Removed unused icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,6 @@ export default function Layout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
             <head>
-                {/* Reference the favicon from the public directory */}
-                <link rel="icon" href="/images/logo.png" />
-                <link rel="apple-touch-icon" href="/images/logo.png" /> {/* Optional: for iOS devices */}
                 <title>Hatix Ntsoa</title>
             </head>
             <body>{children}</body>
