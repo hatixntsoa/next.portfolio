0825e51584d529b342f9685f8331bc779e83e0d2
Hatix Ntsoa <hatixntsoa@gmail.com>
2025-01-04 22:25:57 +0300

    feat: Dynamic Year for the Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,11 @@
-const Footer = () => (
-  <div className="footer text-center w-full">
-    Copyright © 2024 h471x
-  </div>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="footer text-center w-full">
+      Copyright © {currentYear} h471x
+    </div>
+  );
+};
 
 export default Footer;
