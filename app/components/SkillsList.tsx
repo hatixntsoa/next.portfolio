04780e429f5511a012b69678798ea829e4e13c04
Hatix Ntsoa <hatixntsoa@gmail.com>
2025-03-28 01:50:19 +0300

    update: SkillsList Responsiveness

--- app/components/SkillsList.tsx
+++ app/components/SkillsList.tsx
@@ -1,72 +1,74 @@
 const projects = [
   {
-    name: "Problem Solving",
+    name: "WSL2 Forwarder",
+    url: "https://github.com/h471x/wsl2_forwarder",
+    description: "Simple Batch Script to Forward WSL2 Ports.",
+  },
+  {
+    name: "Baiboly JSON",
+    url: "https://github.com/h471x/baiboly_json",
+    description: "A Simple Malagasy Bible App using JSON Data.",
+  },
+  {
+    name: "Git & GitHub CLI",
     url: "https://github.com/h471x/git_gh",
     description: "Simple Automation Scripts.",
   },
   {
-    name: "Python",
+    name: "markdown_previewer",
     url: "https://github.com/h471x/markdown_previewer",
     description: "A markdown note-taking tool.",
   },
   {
-    name: "DevOps",
+    name: "password_generator",
     url: "https://github.com/h471x/password_generator",
-    description: "A Simple CLI Tool to generate passwords.",
+    description: "A Password Generation CLI Tool.",
   },
   {
-    name: "Kali Linux",
+    name: "port_scanner",
     url: "https://github.com/h471x/port_scanner",
     description: "A Simple CLI Tool to scan open ports.",
   },
   {
-    name: "Penetration Testing",
+    name: "web_scraper",
     url: "https://github.com/h471x/web_scraper",
     description: "A Simple CLI Tool to scrap the web.",
   },
   {
-    name: "Cybersecurity",
+    name: "whois_lookup",
     url: "https://github.com/h471x/whois_lookup",
     description: "A Simple CLI Tool to perform whois lookup.",
   },
   {
-    name: "Shell Scripting",
-    url: "https://github.com/h471x/whois_lookup",
-    description: "A Simple CLI Tool to perform whois lookup.",
-  },
-  {
-    name: "Networking",
-    url: "https://github.com/h471x/whois_lookup",
-    description: "A Simple CLI Tool to perform whois lookup.",
-  },
-  {
-    name: "Neovim",
-    url: "https://github.com/h471x/whois_lookup",
-    description: "A Simple CLI Tool to perform whois lookup.",
+    name: "youtube_downloader",
+    url: "https://github.com/h471x/youtube_downloader",
+    description: "Simple YouTube Video Downloader.",
   },
 ];
 
-const SkillsList = () => {
+const ProjectsList = () => {
   return (
     <>
       <h2 className="command-line">  󰔸 ~ cat skills </h2>
       <br />
-      <div className="project-list">
+      <div className="project-list flex flex-col gap-5">
         {projects.map((project) => (
-          <p key={project.name} className="text-xl leading-[1.6] flex items-center">
-            <span className="bullet">●</span>
-            <span className="flex items-center w-full truncate">
-              <a
-                className="custom-link whitespace-nowrap"
-                href={project.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {project.name}
-              </a>
-              <span className="ml-2 whitespace-nowrap">
-                : {project.description}
-              </span>
+          <p key={project.name} className="text-xl leading-[1.6] flex flex-wrap">
+            <a
+              href={project.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="bullet hidden sm:inline">● </span>
+              <span className="project link">{project.name}</span>
+            </a>
+            <span className="
+              project-description
+              inline-block
+              sm:inline w-full
+              sm:w-auto"
+            >
+              {project.description}
             </span>
           </p>
         ))}
@@ -76,4 +78,4 @@ const SkillsList = () => {
   );
 };
 
-export default SkillsList;
+export default ProjectsList;
