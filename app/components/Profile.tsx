5a24819fb2c4634cf9ee4f1f4e90b84ab453d072
Hatix Ntsoa <hatixntsoa@gmail.com>
2025-03-28 01:50:32 +0300

    feat: Animate Profile On Hover

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import Image from 'next/image';
-import hatixImage from '../images/HaintsoN.jpg';
+import mainImage from '../images/HaintsoN.jpg';
+import hoverImage from '../images/logo.gif';
 
 const Profile: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setTimeout(() => {
+      setIsHovered(false);
+    }, 2000);
+  };
+
   return (
     <Image
-      src={hatixImage}
+      src={isHovered ? hoverImage : mainImage}
       alt="Hatix Ntsoa"
       className="round-img"
       width={200}
       height={200}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     />
   );
 };
